8ac304034d04ebd018283eebebee74bc87ce53f6
Joel Villarreal II <joel.villa2329@gmail.com>
2025-11-27 16:03:11 -0700

    added updated MUX messages

--- src/Message/Topic.java
+++ src/Message/Topic.java
@@ -39,4 +39,99 @@ public class Topic {
     public static final int CABIN_LOAD = 205; // eMUX
     public static final int FIRE_KEY = 206; // eMUX
 
+    // List of topics
+    public static final int systemStop = 1;
+    public static final int systemStart = 2;
+    public static final int systemReset = 3;
+    public static final int clearFire = 4;
+    public static final int elevatorMode = 5;
+    public static final int startElevator = 6;
+    public static final int stopElevator = 7;
+
+    // Device relevant control
+    public static final int doorControl = 100;
+    public static final int carDispatch = 102;
+    public static final int carStop = 103; //TODO
+    public static final int resetFloorSelection = 109;
+    public static final int resetCall = 110;
+    public static final int displayFloor = 111;
+    public static final int displayDirection = 112;
+
+    public static final int callsEnable = 113;
+    public static final int selectionsEnable = 114;
+    public static final int selectionsType = 115;
+    public static final int playSound = 116;
+
+    public static final int fireAlarm = 120;
+
+    public static final int hallCall = 200;
+
+    public static final int cabinSelect = 201;
+    public static final int cabinPosition = 202;
+    public static final int doorSensor = 203;
+    public static final int doorStatus = 204;
+    public static final int cabinLoad = 205;
+    public static final int fireKey = 206;
+    public static final int currDirection = 207; //TODO: Useless maybe?
+    public static final int currMovement = 208; //TODO
+    public static final int fireAlarmActive = 209;
+
+    // Control devices
+    public static final int buttons = 300;
+    public static final int cabin = 301;
+    public static final int doorAssembly = 302;
+    public static final int mode = 303;
+    public static final int notifier = 304;
+
+    // List of Subtopics
+    public static final int allElevators = 0;
+    public static final int elevatorOne = 1;
+    public static final int elevatorTwo = 2;
+    public static final int elevatorThree = 3;
+    public static final int elevatorFour = 4;
+
+    public static final int floorOne = 1;
+    public static final int floorTwo = 2;
+    public static final int floorThree = 3;
+    public static final int floorFour = 4;
+    public static final int floorFive = 5;
+    public static final int floorSix = 6;
+    public static final int floorSeven = 7;
+    public static final int floorEight = 8;
+    public static final int floorNine = 9;
+    public static final int floorTen = 10;
+
+    // List of bodies
+    public static final int emptyBody = 0;
+    public static final int centralized = 1000;
+    public static final int independent = 1100;
+    public static final int fire = 1110;
+
+    public static final int doorOpen = 0;
+    public static final int doorClose = 1;
+
+    public static final int deviceFire = 0;
+    public static final int deviceIndependent = 1;
+    public static final int deviceCentralized = 2;
+
+    public static final int deviceFloorOne = 1;
+    public static final int deviceFloorTwo = 2;
+    public static final int deviceFloorThree = 3;
+    public static final int deviceFloorFour = 4;
+    public static final int deviceFloorFive = 5;
+    public static final int deviceFloorSix = 6;
+    public static final int deviceFloorSeven = 7;
+    public static final int deviceFloorEight = 8;
+    public static final int deviceFloorNine = 9;
+    public static final int deviceFloorTen = 10;
+
+    public static final int up = 0;
+    public static final int down = 1;
+    public static final int none = 2;
+
+    public static final int obstructed = 0;
+    public static final int clear = 1;
+
+    public static final int normal = 0;
+    public static final int overloaded = 1;
 }
