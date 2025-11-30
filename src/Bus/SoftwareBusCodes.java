45213adb5953c9e1782e2c49116a6e7db7587754
Joel Villarreal II <joel.villa2329@gmail.com>
2025-11-30 14:28:11 -0700

    EC to CC and CC to EC topics

--- src/Bus/SoftwareBusCodes.java
+++ src/Bus/SoftwareBusCodes.java
@@ -1,94 +1,125 @@
 package Bus;
 
+/**
+ * All the topics, subtopics, and bodies
+ */
 public class SoftwareBusCodes {
-    //List of topics
-    public static int systemStop = 1;
-    public static int systemStart = 2;
-    public static int clearFire = 4;
-    public static int elevatorMode = 5;
-    public static int startElevator = 6;
-    public static int stopElevator = 7;
+    // List of topics
+    public static final int systemStop = 1;
+    public static final int systemStart = 2;
+    public static final int systemReset = 3;
+    public static final int clearFire = 4;
+    public static final int elevatorMode = 5;
+    public static final int startElevator = 6;
+    public static final int stopElevator = 7;
 
-    //Device relevant control
+    // Elevator Controller to MUX
+    public static final int doorControl = 100;
+    public static final int carDispatch = 102;
+    public static final int carStop = 103;
+    public static final int resetFloorSelection = 109;
+    public static final int resetCall = 110;
+    public static final int displayFloor = 111;
+    public static final int displayDirection = 112;
 
-    public static int doorControl = 100;
-    public static int carDispatch = 102;
-    public static int setMode = 103;
-    public static int resetFloorSelection = 109;
-    public static int resetCall = 110;
-    public static int displayFloor = 111;
-    public static int displayDirection = 112;
+    public static final int callsEnable = 113;
+    public static final int selectionsEnable = 114;
+    public static final int selectionsType = 115;
+    public static final int playSound = 116;
 
-    public static int fireAlarm = 120;
+    public static final int fireAlarm = 120;
 
-    public static int hallCall = 200;
+    // MUX to elevator controller
+    public static final int hallCall = 200;
 
-    public static int cabinSelect = 201;
-    public static int cabinPosition = 202;
-    public static int doorSensor = 203;
-    public static int doorStatus = 204;
-    public static int cabinLoad = 205;
-    public static int fireKey = 206;
+    public static final int cabinSelect = 201;
+    public static final int cabinPosition = 202;
+    public static final int doorSensor = 203;
+    public static final int doorStatus = 204;
+    public static final int cabinLoad = 205;
+    public static final int fireKey = 206;
+    public static final int currDirection = 207;
+    public static final int currMovement = 208;
+    public static final int fireAlarmActive = 209;
+    public static final int topSensor = 210;
+    public static final int bottomSensor = 211;
 
-    //Control devices
-    public static int buttons = 300;
-    public static int cabin = 301;
-    public static int doorAssembly = 302;
-    public static int mode = 303;
-    public static int notifier = 304;
-
-
-    //List of Subtopics
-    public static int allElevators = 0;
-    public static int elevatorOne = 1;
-    public static int elevatorTwo = 2;
-    public static int elevatorThree = 3;
-    public static int elevatorFour = 4;
-
-    public static int floorOne = 1;
-    public static int floorTwo = 2;
-    public static int floorThree = 3;
-    public static int floorFour = 4;
-    public static int floorFive = 5;
-    public static int floorSix = 6;
-    public static int floorSeven = 7;
-    public static int floorEight = 8;
-    public static int floorNine = 9;
-    public static int floorTen = 10;
-
-    //List of bodies
-    public static int emptyBody = 0;
-    public static int centralized = 1000;
-    public static int independent = 1100;
-    public static int fire = 1110;
-
-    public static int doorOpen = 0;
-    public static int doorClose = 1;
-
-    public static int deviceFire = 0;
-    public static int deviceIndependent = 1;
-    public static int deviceCentralized = 2;
-
-    public static int deviceFloorOne = 1;
-    public static int deviceFloorTwo = 2;
-    public static int deviceFloorThree = 3;
-    public static int deviceFloorFour = 4;
-    public static int deviceFloorFive = 5;
-    public static int deviceFloorSix = 6;
-    public static int deviceFloorSeven = 7;
-    public static int deviceFloorEight = 8;
-    public static int deviceFloorNine = 9;
-    public static int deviceFloorTen = 10;
-
-    public static int up = 0;
-    public static int down = 1;
-    public static int none = 2;
-
-    public static int obstructed = 0;
-    public static int clear = 1;
-
-    public static int normal = 0;
-    public static int overloaded = 1;
+    // Command Center to Elevator Controller
+    public static final int setMode = 300;
+    public static final int setDestination = 301;
+    public static final int elevatorOnOff = 303;
 
 
+
+    // Elevator Controller to Command Center
+    public static final int atFloor = 400;
+    public static final int fireMode = 401;
+    public static final int elevatorStatus = 402;
+    public static final int doorStatusCC = 403;
+    public static final int doorSensorCC = 404;
+
+    // Control devices
+    public static final int buttons = 300;
+    public static final int cabin = 301;
+    public static final int doorAssembly = 302;
+    public static final int mode = 303;
+    public static final int notifier = 304;
+
+    // List of Subtopics
+    public static final int allElevators = 0;
+    public static final int elevatorOne = 1;
+    public static final int elevatorTwo = 2;
+    public static final int elevatorThree = 3;
+    public static final int elevatorFour = 4;
+
+    public static final int floorOne = 1;
+    public static final int floorTwo = 2;
+    public static final int floorThree = 3;
+    public static final int floorFour = 4;
+    public static final int floorFive = 5;
+    public static final int floorSix = 6;
+    public static final int floorSeven = 7;
+    public static final int floorEight = 8;
+    public static final int floorNine = 9;
+    public static final int floorTen = 10;
+
+    // List of bodies
+    public static final int emptyBody = 0;
+    public static final int centralized = 1000;
+    public static final int independent = 1100;
+    public static final int fire = 1110;
+
+    public static final int doorOpen = 0;
+    public static final int doorClose = 1;
+
+    public static final int deviceFire = 0;
+    public static final int deviceIndependent = 1;
+    public static final int deviceCentralized = 2;
+
+    public static final int deviceFloorOne = 1;
+    public static final int deviceFloorTwo = 2;
+    public static final int deviceFloorThree = 3;
+    public static final int deviceFloorFour = 4;
+    public static final int deviceFloorFive = 5;
+    public static final int deviceFloorSix = 6;
+    public static final int deviceFloorSeven = 7;
+    public static final int deviceFloorEight = 8;
+    public static final int deviceFloorNine = 9;
+    public static final int deviceFloorTen = 10;
+
+    public static final int up = 0;
+    public static final int down = 1;
+    public static final int none = 2;
+
+    public static final int obstructed = 0;
+    public static final int clear = 1;
+
+    public final static int inactive = 0;
+    public final static int active = 1;
+
+    public static final int normal = 0;
+    public static final int overloaded = 1;
+
+    public final static int downOffset = 0; // for down call button body:  1 - 10
+    public final static int upOffset = 100; // for up call button body: 101 - 110
 }
