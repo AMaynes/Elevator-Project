596eefa36ed3a818b062aee69616901bd308ada7
Joel Villarreal II <joel.villa2329@gmail.com>
2025-11-30 21:58:59 -0700

    edited constants for clarity

--- src/ElevatorController/LowerLevel/Notifier.java
+++ src/ElevatorController/LowerLevel/Notifier.java
@@ -15,24 +15,27 @@ import Message.Message;
 //Todo: what am I supposed to do with overloaded here?
     // Seems like the MUX should be telling us overloaded?
 public class Notifier {
-    private int elevatorID;
-    private SoftwareBus softwareBus;
+    private final int ELEVATOR_ID;
+    private final SoftwareBus softwareBus;
 
-    // Topic for car postion
+    // ***Topics ***
+    // To MUX
     private static final int TOPIC_CABIN_POSITION = SoftwareBusCodes.cabinPosition;
     private static final int TOPIC_DISPLAY_DIRECTION =SoftwareBusCodes.displayDirection;
     private static final int TOPIC_PLAY_SOUND = SoftwareBusCodes.playSound;
+
+    // To Command Center
     private static final int TOPIC_COMMAND_CENTER =
             SoftwareBusCodes.elevatorStatus;
     private static final int TOPIC_COMMAND_DIRECTION =
             SoftwareBusCodes.ccElevatorDirection;
 
     //bodies
-    private static final int BODY_ARRIVAL = 0;
-    private static final int BODY_OVERLOAD = 1;
+    private static final int BODY_ARRIVAL = SoftwareBusCodes.arrivalNoise;
+    private static final int BODY_OVERLOAD = SoftwareBusCodes.overloadNoise;
 
     public  Notifier(int elevatorID, SoftwareBus softwareBus){
-        this.elevatorID = elevatorID;
+        this.ELEVATOR_ID = elevatorID;
         this.softwareBus = softwareBus;
     }
 
@@ -42,7 +45,7 @@ public class Notifier {
      * @param floorNDirection This elevator's current floor and direction
      */
     public void arrivedAtFloor(FloorNDirection floorNDirection){
-        softwareBus.publish(new Message(TOPIC_PLAY_SOUND, elevatorID, BODY_ARRIVAL));
+        softwareBus.publish(new Message(TOPIC_PLAY_SOUND, ELEVATOR_ID, BODY_ARRIVAL));
     }
 
     /**
@@ -54,16 +57,16 @@ public class Notifier {
         int direction = floorNDirection.getDirection().getIntegerVersion();
 
         // Tell mux what direction we're going
-        softwareBus.publish(new Message(TOPIC_DISPLAY_DIRECTION,elevatorID,direction));
+        softwareBus.publish(new Message(TOPIC_DISPLAY_DIRECTION, ELEVATOR_ID,direction));
 
         //Tell mux where we are
-        softwareBus.publish(new Message(TOPIC_CABIN_POSITION,elevatorID, floorNDirection.floor()));
+        softwareBus.publish(new Message(TOPIC_CABIN_POSITION, ELEVATOR_ID, floorNDirection.floor()));
 
         //Tell CC where we are
-        softwareBus.publish(new Message(TOPIC_COMMAND_CENTER,elevatorID,
+        softwareBus.publish(new Message(TOPIC_COMMAND_CENTER, ELEVATOR_ID,
                 floorNDirection.floor()));
 
-        softwareBus.publish(new Message(TOPIC_COMMAND_DIRECTION, elevatorID,
+        softwareBus.publish(new Message(TOPIC_COMMAND_DIRECTION, ELEVATOR_ID,
                 direction));
     }
 
@@ -71,15 +74,15 @@ public class Notifier {
      * Notify the MUX to play the capacity buzzer
      */
     public void playCapacityNoise(){
-        softwareBus.publish(new Message(TOPIC_PLAY_SOUND,elevatorID,
-                SoftwareBusCodes.overloaded ));
+        softwareBus.publish(new Message(TOPIC_PLAY_SOUND, ELEVATOR_ID,
+                BODY_OVERLOAD));
     }
 
     /**
      * Notify the MUX to stop playing the capacity buzzer
      */
     public void stopCapacityNoise(){
-        // NEVER HAPPENS will end on its own
+        // NEVER HAPPENS, capacity nose plays for a set duration and stops on its own
     }
 
 }
